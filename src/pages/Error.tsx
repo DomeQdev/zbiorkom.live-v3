64322aad7c467c4579fac3ea52cff41172deed6d
♿ <domeqdev@pm.me>
2022-06-14 14:06:27 +0200

    Official bug fix from @torvalds.
    Co-Authored-By: Linus Torvalds <1024025+torvalds@users.noreply.github.com>

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 export default ({ text, message }: { text: string, message?: string }) => {
     const navigate = useNavigate();
+    
     return <div style={{ textAlign: "center", marginTop: 50 }}>
         <img src="/img/logo192.png" width="128" height="128" />
         <h1>{text}</h1>
