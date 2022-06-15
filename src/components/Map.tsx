cae92362a10650934b970c1ce322f9507422e6de
♿ <domeqdev@pm.me>
2022-06-15 18:17:04 +0200

    Fix map not being displayed

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import Map, { GeolocateControl, NavigationControl, Marker } from 'react-map-gl';
+import Map, { GeolocateControl, NavigationControl } from 'react-map-gl';
 import cities from "../cities.json";
 
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -14,13 +14,12 @@ export default ({ city, location, children }: { city: keyof typeof cities, locat
         }}
         minZoom={8}
         minPitch={10}
-        style={{ width: "100%", height: "calc(100vh - 64px)", overflow: "hidden" }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken="pk.eyJ1IjoiZG9tZXEiLCJhIjoiY2t6c2JnZnp5MDExMzJ4bWlpMjcwaDR0dCJ9.v2ONdyf7WN70xFwUOyUuXQ" // idk if it's should be here, but who cares :)
+        mapboxAccessToken="pk.eyJ1IjoiZG9tZXEiLCJhIjoiY2t6c2JnZnp5MDExMzJ4bWlpMjcwaDR0dCJ9.v2ONdyf7WN70xFwUOyUuXQ"
         attributionControl={false}
     >
-        <NavigationControl visualizePitch />
-        <GeolocateControl trackUserLocation showUserHeading showUserLocation positionOptions={{ enableHighAccuracy: true }} />
+        <NavigationControl visualizePitch position='bottom-right' />
+        <GeolocateControl trackUserLocation showUserHeading showUserLocation positionOptions={{ enableHighAccuracy: true }} position='bottom-right' />
         {children}
     </Map>
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+console.log('%czbiorkom.live', 'color: #5aa159;font-size:50px;');
+console.log('%cTwórca: DomeQ <info@domeq.pw>\nGitHub: https://github.com/DomeQdev/zbiorkom.live\nDiscord: https://discord.gg/QYRswCH6Gw', 'font-weight:bold;color:#5aa159');
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
