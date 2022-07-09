a620c3b9168edffa99c60ef64e2b4ceb2bf38b7d
DomeQ <domeqdev@pm.me>
2022-07-09 17:09:41 +0200

    fix bug

--- src/pages/Vehicle.tsx
+++ src/pages/Vehicle.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { BottomSheet } from "react-spring-bottom-sheet";
 import { toast } from "react-toastify";
 import { useMap } from "react-map-gl";
-import { realtime } from "../util/realtime";
+import { RealTime, RealTimeResponse } from "../util/realtime";
 import { Trip, City, Vehicle } from "../util/typings";
 import VehicleMarker from "../components/VehicleMarker";
 import Shapes from "../components/Shapes";
@@ -12,40 +12,42 @@ import cities from "../cities.json";
 export default ({ city, vehicle, mapBearing }: { city: City, vehicle: Vehicle, mapBearing: number }) => {
     const navigate = useNavigate();
     const { current: map } = useMap();
-    const cityData = cities[city];
-
     const [follow, setFollow] = useState<boolean>(true);
     const [trip, setTrip] = useState<Trip>();
+    const [realTime, setRealTime] = useState<RealTimeResponse>();
+    const cityData = cities[city];
 
     useEffect(() => {
         map?.on("movestart", (e) => e.originalEvent && e.originalEvent.type !== "resize" ? setFollow(false) : undefined);
     }, []);
 
     useEffect(() => {
-        if(!follow) return;
+        if (!follow) return;
         map?.flyTo({
             center: vehicle._location
         });
     }, [vehicle._location, follow]);
 
     useEffect(() => {
-        if(!vehicle.trip) return;
+        if (!vehicle.trip) return;
         fetch(cityData.api.trip.replace("{{trip}}", encodeURIComponent(vehicle.trip))).then(res => res.json()).then(setTrip).catch((e) => {
             toast.error(`Fatalny błąd.`);
             console.error(e);
         });
     }, [vehicle.trip]);
 
-    const RT = trip ? useMemo(() => realtime({
-        trip,
-        location: vehicle._location,
-        delay: vehicle.delay
-    }), [vehicle]) : null;
-    console.log(RT)
+    useEffect(() => {
+        if (!trip) return;
+        setRealTime(RealTime({
+            trip,
+            location: vehicle._location,
+            delay: vehicle.delay
+        }))
+    }, [vehicle]);
 
     return <>
         <VehicleMarker vehicle={vehicle} mapBearing={mapBearing} />
-        {trip && <Shapes trip={trip} delay={RT?.delay} />}
+        {trip && <Shapes trip={trip} delay={realTime?.delay} />}
         <BottomSheet
             open
             onDismiss={() => navigate(".")}
--- src/util/realtime.ts
+++ src/util/realtime.ts
@@ -1,11 +1,18 @@
 import { nearestPointOnLine, point } from "@turf/turf";
 import { Trip, TripStop, Vehicle } from "./typings";
 
-const realtime = ({ trip, location, delay }: {
+type RealTimeResponse = {
+    servingIndex: number | null,
+    nextStopIndex: number,
+    delay: number,
+    travelledToNextStop: number
+};
+
+const RealTime = ({ trip, location, delay }: {
     trip: Trip,
     location: Vehicle["_location"],
     delay?: number
-}) => {
+}): RealTimeResponse => {
     let vehicleDistance = nearestPointOnLine(trip.shapes, point(location), { units: 'meters' }).properties.location || 0;
     let stops = trip.stops.map(stop => ({
         ...stop,
@@ -20,22 +27,20 @@ const realtime = ({ trip, location, delay }: {
     let nextStopIndex = stops.findIndex(stop => stop?.metersToStop > 50) || stops.length - 1;
 
     let cur = serving || lastStop;
-    let realtime = ((nextStop.arrival - cur.departure) * percentTravelled(cur, nextStop)) - (nextStop.arrival - Date.now());
+    let travelledToNextStop = percentTravelled(cur, nextStop);
+    let realtime = ((nextStop.arrival - cur.departure) * travelledToNextStop) - (nextStop.arrival - Date.now());
     let _delay = delay || (tripStart > 0 ? realtime : 0);
 
     return {
-        lastStop,
-        serving,
-        nextStop,
         servingIndex: tripStart > 0 ? (servingIndex === -1 ? null : servingIndex) : 0,
         nextStopIndex: tripStart > 0 ? nextStopIndex : 1,
-        delay: Math.floor(_delay)
+        delay: Math.floor(_delay),
+        travelledToNextStop
     };
 };
 
-export {
-    realtime
-};
+export type { RealTimeResponse }
+export { RealTime };
 
 function percentTravelled(stop1: TripStop, stop2: TripStop) {
     let res = stop1.metersToStop / (stop1.metersToStop - stop2.metersToStop);
