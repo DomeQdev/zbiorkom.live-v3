b2812bcb58b08293bcf0f82aa291f45c5949178d
DomeQ <domeqdev@pm.me>
2022-08-07 19:16:14 +0000

    s

--- src/components/PlaceSearch.tsx
+++ src/components/PlaceSearch.tsx
@@ -1,4 +1,4 @@
-import { TextField, AppBar, Toolbar, IconButton, InputAdornment, List, ListItemButton, ListItemAvatar, ListItemText, Avatar, Divider, ListItem, Skeleton } from "@mui/material";
+import { TextField, AppBar, Toolbar, IconButton, InputAdornment, List, ListItemButton, ListItemAvatar, ListItemText, Avatar, Divider, Skeleton } from "@mui/material";
 import { ArrowBack, DirectionsTransit, HighlightOff, NoTransfer } from "@mui/icons-material";
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
@@ -80,7 +80,7 @@ export default ({ city, placeholder, onData }: { city: City, placeholder: string
         </List> : <div style={{ textAlign: "center" }}>
             <NoTransfer color="primary" sx={{ width: 70, height: 70 }} /><br />
             <b style={{ fontSize: 18 }}>Nie znaleziono wyników.</b>
-        </div> : input && input.length >= 3 ? new Array(5).fill(null).map<React.ReactNode>((i) => <ListItem key={`1-${i}`}>
+        </div> : input && input.length >= 3 ? new Array(5).fill(null).map<React.ReactNode>((i) => <ListItemButton key={`1-${i}`}>
             <ListItemAvatar>
                 <Skeleton variant="circular">
                     <Avatar />
@@ -89,6 +89,6 @@ export default ({ city, placeholder, onData }: { city: City, placeholder: string
             <ListItemText>
                 <Skeleton variant="text" width={250} height={24} />
             </ListItemText>
-        </ListItem>).reduce((prev, curr, i) => [prev, <Divider variant="inset" key={`1_${i}`} />, curr]) : "not searching mode"}
+        </ListItemButton>).reduce((prev, curr, i) => [prev, <Divider variant="inset" key={`1_${i}`} />, curr]) : "wpisz 3 znaki aby cos wyszukac"}
     </>;
 };
