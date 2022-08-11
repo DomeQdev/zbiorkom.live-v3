96acbc809b035bbe7d8855bc7048c64ddc876499
♿ <domeqdev@pm.me>
2022-08-11 21:48:06 +0200

    Update result.ts

--- functions/api/planner/result.ts
+++ functions/api/planner/result.ts
@@ -8,11 +8,11 @@ export const onRequestPatch = async ({ request, env }) => {
         }
     });
 
-    let routes = env.ZBIORKOM?.get(key, { type: "text" });
+    let routes = env.ZBIORKOM?.get(key, { type: "json" });
     if (!routes) return new Response(JSON.stringify({ error: "Routes not found" }), { status: 404 });
 
     let route = routes[id];
-    if (!route) return new Response(JSON.stringify({ error: "Route not found" }), { status: 404 });
+    if (!route) return new Response(JSON.stringify({ error: "Route not found", routes }), { status: 404 });
 
     return new Response(JSON.stringify(route), {
         headers: {
