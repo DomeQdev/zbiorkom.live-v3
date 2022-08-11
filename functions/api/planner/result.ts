48f816ecf27479a7a61840e5053ffc947e850f4d
♿ <domeqdev@pm.me>
2022-08-11 21:57:01 +0200

    Update result.ts

--- functions/api/planner/result.ts
+++ functions/api/planner/result.ts
@@ -1,6 +1,6 @@
 export const onRequestPatch = async ({ request, env }) => {
     const { key, id }: { key: string, id: number } = await request.json();
-    if (!key || !id) return new Response(JSON.stringify({ error: "Provide key=string, id=number" }), {
+    if (!key || (!id && id !== 0)) return new Response(JSON.stringify({ error: "Provide key=string, id=number" }), {
         status: 400,
         headers: {
             "Content-Type": "application/json",
@@ -8,10 +8,10 @@ export const onRequestPatch = async ({ request, env }) => {
         }
     });
 
-    let routes = env.ZBIORKOM?.get(key, { type: "json" });
+    let routes = env.ZBIORKOM?.get(key);
     if (!routes) return new Response(JSON.stringify({ error: "Routes not found" }), { status: 404 });
 
-    let route = routes[id];
+    let route = JSON.stringify(routes)[id];
     if (!route) return new Response(JSON.stringify({ error: "Route not found", routes }), { status: 404 });
 
     return new Response(JSON.stringify(route), {
