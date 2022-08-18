23a3a00668a5a407e719d134a8c0cec632e53810
♿ <domeqdev@pm.me>
2022-08-18 13:38:29 +0200

    .

--- src/pages/Brigades.tsx
+++ src/pages/Brigades.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Dialog, DialogTitle, DialogContent, ToggleButton, IconButton, Slide } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, ToggleButton, IconButton, Slide, Skeleton, Grid } from "@mui/material";
 import { Close } from "@mui/icons-material";
 import { toast } from "react-hot-toast";
 import { City, Route } from "../util/typings";
 import { getData } from "../util/api";
-import { Backdrop } from "../components/Suspense";
 import { Icon } from "../components/Icons";
 import styled from "@emotion/styled";
 import isDark from "../util/isDark";
@@ -15,11 +14,11 @@ const Line = styled(ToggleButton)((props: {
     textcolor: string,
     backgroundcolor: string
 }) => ({
-    width: 110,
-    height: 50,
+    width: 100,
+    height: 40,
     fontSize: 20,
     margin: 5,
-    borderRadius: 15,
+    borderRadius: 25,
     color: darkMode ? props.textcolor : props.backgroundcolor,
     backgroundColor: darkMode ? props.backgroundcolor : props.textcolor,
     borderColor: darkMode ? props.textcolor : props.backgroundcolor,
@@ -53,10 +52,10 @@ export default ({ city }: { city: City }) => {
         });
     }, [state]);
 
-    return routes?.length ? <div style={{ textAlign: "center" }}>
+    return <div style={{ textAlign: "center", marginLeft: "auto", marginRight: "auto", width: "90%" }}>
         <h1>Rozkład brygad</h1>
         <p>Wybierz linię:</p>
-        {routes?.map(route => <Line
+        {routes?.length ? routes.map(route => <Line
             value={route.line}
             key={route.line}
             textcolor={route.text === route.color ? "#fff" : route.text}
@@ -66,8 +65,8 @@ export default ({ city }: { city: City }) => {
                 navigate(".", { state: route.line });
             }}
         >
-            <Icon type={route.type} style={{ width: 21, height: 21 }} />&nbsp;{route.line.replace("-", "")}
-        </Line>)}
+            <Icon type={route.type} style={{ width: 21, height: 21 }} />&nbsp;{route.line}
+        </Line>) : <Grid container justifyContent="center">{new Array(30).fill(null).map((_, i) => <Skeleton key={`1_${i}`} variant="rounded" width={100} height={40} sx={{ margin: "5px", borderRadius: "15px" }} />)}</Grid>}
 
         <Dialog
             open={!!state}
@@ -78,15 +77,15 @@ export default ({ city }: { city: City }) => {
         >
             <DialogTitle style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}><span>Wybierz brygadę dla linii <b>{state as string}</b></span><IconButton onClick={() => navigate(".", { state: null, replace: true })}><Close /></IconButton></DialogTitle>
             <DialogContent dividers>
-                {selectedBrigades ? <div style={{ textAlign: "center" }}>
-                    {selectedBrigades.length ? selectedBrigades.map(brigade => <ToggleButton
+                <div style={{ textAlign: "center" }}>
+                    {selectedBrigades ? selectedBrigades.length ? selectedBrigades.map(brigade => <ToggleButton
                         value={brigade}
                         key={brigade}
                         onClick={() => navigate(`../brigade/${state}/${brigade}`)}
                         sx={{ width: 70, height: 40, fontSize: 18, margin: 0.5 }}
-                    >{brigade}</ToggleButton>) : <h4>Nie mogliśmy znaleźć rozkładu dla tej linii...</h4>}
-                </div> : <Backdrop />}
+                    >{brigade}</ToggleButton>) : <h4>Nie mogliśmy znaleźć rozkładu dla tej linii...</h4> : <Grid container justifyContent="center">{new Array(20).fill(null).map((_, i) => <Skeleton key={`1_${i}`} variant="rounded" width={70} height={40} sx={{ margin: 0.5 }} />)}</Grid>}
+                </div>
             </DialogContent>
         </Dialog>
-    </div> : <Backdrop />;
+    </div>;
 };
