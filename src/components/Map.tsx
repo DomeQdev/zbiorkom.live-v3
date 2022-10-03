2ab9c59b09d6adab59116624c15fffd1d5a0534f
♿ <domeqdev@pm.me>
2022-10-03 19:31:44 +0200

    remove google maps support

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -19,7 +19,7 @@ export default ({ city, location, style, children }: { city: City, location?: [n
         }}
         minZoom={2}
         maxPitch={0}
-        mapStyle={mapStyles[mapStyle].style as string | Style}
+        mapStyle={mapStyles[mapStyle]?.style as string | Style || mapStyles["ms"].style}
         mapboxAccessToken="pk.eyJ1IjoiZG9tZXEiLCJhIjoiY2t6c2JnZnp5MDExMzJ4bWlpMjcwaDR0dCJ9.v2ONdyf7WN70xFwUOyUuXQ"
         attributionControl={false}
         style={style}
