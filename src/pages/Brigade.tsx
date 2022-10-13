f686d4bc9fb50523c5e37f85a78473db987917e1
DomeQ <domeqdev@pm.me>
2022-10-13 14:38:17 +0200

    Update Brigade.tsx

--- src/pages/Brigade.tsx
+++ src/pages/Brigade.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { List, ListItemButton, ListItemText, Divider, Button, Typography, Box, Skeleton, ListItem } from "@mui/material";
 import { ArrowBack } from "@mui/icons-material";
 import { toast } from "react-hot-toast";
@@ -34,15 +34,17 @@ export default ({ city }: { city: City }) => {
         <h1 style={{ fontWeight: "normal" }}>Rozkład brygady <b>{line}</b>/{brigade}</h1>
         <Button variant="outlined" startIcon={<ArrowBack />} onClick={() => window.history.back()}>Wróć</Button>
 
-        {schedule ? (schedule.length ? <List>{schedule.map<React.ReactNode>(sched => <ListItemButton key={sched.trip}>
-            <ListItemText primary={<Typography noWrap>{sched.headsign}</Typography>} secondary={<>z przystanku {sched.firstStop}</>} />
-            <span style={{ textAlign: "right" }}>
-                <ListItemText
-                    primary={<><span style={{ textDecoration: sched.realStart ? "line-through" : "" }}>{timeString(sched.start)}</span> {sched.realStart ? <span style={{ color: "red" }}>{timeString(sched.realStart)}</span> : null}</>}
-                    secondary={<><span style={{ textDecoration: sched.realEnd ? "line-through" : "" }}>{timeString(sched.end)}</span> {sched.realEnd ? <span style={{ color: "red" }}>{timeString(sched.realEnd)}</span> : null}</>}
-                />
-            </span>
-        </ListItemButton>).reduce((prev, curr, i) => [prev, <Divider key={i} textAlign="left" style={{ color: "#9ba1ab", fontSize: 14 }}>{(schedule[i].start - schedule[i - 1]!.end) / 60000 < 60 ? schedule[i].realStart ? <span style={{ color: "red" }}>Brak postoju z powodu zbyt dużego opóźnienia.</span> : <>Postój {Math.floor((schedule[i].start - schedule[i - 1]!.end) / 60000)} min</> : null}</Divider>, curr])}</List> : <h4>Nie mogliśmy znaleźć rozkładu dla tej brygady...</h4>) : <List>
+        {schedule ? (schedule.length ? <List>
+            {schedule.map<React.ReactNode>(sched => <ListItemButton key={sched.trip} component={Link} to={`../trip?trip=${sche.trip}`}>
+                <ListItemText primary={<Typography noWrap>{sched.headsign}</Typography>} secondary={<>z przystanku {sched.firstStop}</>} />
+                <span style={{ textAlign: "right" }}>
+                    <ListItemText
+                        primary={<><span style={{ textDecoration: sched.realStart ? "line-through" : "" }}>{timeString(sched.start)}</span> {sched.realStart ? <span style={{ color: "red" }}>{timeString(sched.realStart)}</span> : null}</>}
+                        secondary={<><span style={{ textDecoration: sched.realEnd ? "line-through" : "" }}>{timeString(sched.end)}</span> {sched.realEnd ? <span style={{ color: "red" }}>{timeString(sched.realEnd)}</span> : null}</>}
+                    />
+                </span>
+            </ListItemButton>).reduce((prev, curr, i) => [prev, <Divider key={i} textAlign="left" style={{ color: "#9ba1ab", fontSize: 14 }}>{(schedule[i].start - schedule[i - 1]!.end) / 60000 < 60 ? schedule[i].realStart ? <span style={{ color: "red" }}>Brak postoju z powodu zbyt dużego opóźnienia.</span> : <>Postój {Math.floor((schedule[i].start - schedule[i - 1]!.end) / 60000)} min</> : null}</Divider>, curr])}
+        </List> : <h4>Nie mogliśmy znaleźć rozkładu dla tej brygady...</h4>) : <List>
             {new Array(15).fill(0).map<React.ReactNode>((_, i) => <ListItem key={i}><ListItemText primary={<Skeleton variant="text" width={100} />} secondary={<Skeleton variant="text" width={150} />} /><ListItemText sx={{ right: 0, position: "absolute" }} primary={<Skeleton variant="text" width={60} sx={{ textAlign: "right" }} />} secondary={<Skeleton variant="text" width={60} />} /></ListItem>).reduce((prev, curr, i) => [prev, <Divider key={`1-${i}`} />, curr])}
         </List>}
     </Box>;
