b320b4d2abf94708f5efacfebd74fe35aacfa0fb
♿ <domeqdev@pm.me>
2022-10-27 08:09:58 +0200

    update ads

--- src/util/isDark.ts
+++ src/util/isDark.ts
@@ -1,13 +1,1 @@
-export default () => {
-    let ls = localStorage.getItem("theme");
-    if (!ls) {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            localStorage.setItem("theme", "dark");
-            return true;
-        } else {
-            localStorage.setItem("theme", "light");
-            return false;
-        }
-    }
-    return ls === "dark";
-};
+export default () => localStorage.getItem("theme") ? localStorage.getItem("theme") === "dark" : window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
